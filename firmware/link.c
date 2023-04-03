f98bdbc755c25b47e8878f517d9db996ae7eb618
bricktrooper <kylepinto1018@gmail.com>
2023-04-03 04:24:03 -0400

    Protocol

---
+++ firmware/link.c
@@ -0,0 +1,53 @@
+#include "link.h"
+
+#include "uart.h"
+
+#define MAGIC   0xBEEF
+
+void link_receive(Header * header, void * data)
+{
+	do
+	{
+		U16 byte = ~MAGIC;
+
+		while (byte != MAGIC)
+		{
+			uart_receive(byte, 1);
+		}
+
+		uart_receive((U8 *)header + sizeof(header->magic), sizeof(Header) - sizeof(header->magic));
+		uart_receive(data, header->length);
+	}
+	while (link_checksum(header, data) != header->checksum);
+}
+
+void link_transmit(Opcode opcode, void * data, U8 length)
+{
+	Header header;
+	header.magic = MAGIC;
+	header.opcode = opcode;
+	header.length = length;
+	header.checksum = link_checksum(&header, data);
+
+	uart_transmit(&header, sizeof(Header));
+	uart_transmit(data, length);
+}
+
+U8 link_checksum(Header * header, void * data)
+{
+	U8 checksum = header->checksum;   // cancel out XOR of checksum
+
+	for (U8 i = 0; i < sizeof(Header); i++)
+	{
+		U8 * bytes = (U8 *)header;
+		checksum ^= bytes[i];
+	}
+
+	for (U8 i = 0; i < length; i++)
+	{
+		U8 * bytes = (U8 *)data;
+		checksum ^= bytes[i];
+	}
+
+	return checksum;
+}
---
+++ firmware/link.h
@@ -0,0 +1,30 @@
+#ifndef LINK_H
+#define LINK_H
+
+#include "types.h"
+
+typedef enum Opcode Opcode;
+
+typedef struct Header Header;
+typedef struct Packet Packet;
+
+enum Opcode
+{
+	OPCODE_PING    = 0x00,
+	OPCODE_RGB     = 0x01,
+	OPCODE_RAINBOW = 0x02
+} __attribute__((packed));
+
+struct Header
+{
+	U16 magic;
+	U8 opcode;
+	U8 length;
+	U8 checksum;
+} __attribute__((packed));
+
+void link_receive(Header * header, void * data);
+void link_transmit(Opcode opcode, void * data, U8 length);
+void link_checksum(Header * header, void * data);
+
+#endif /* LINK_H */
