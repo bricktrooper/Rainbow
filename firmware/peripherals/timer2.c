7510b6a16a4f9d1406b7b8d83acd6baf63b0f7c8
bricktrooper <kylepinto1018@gmail.com>
2023-07-17 23:46:35 -0400

    Timer2 interrupt enable function

--- firmware/peripherals/timer2.c
+++ firmware/peripherals/timer2.c
@@ -4,15 +4,20 @@
 
 void timer2_initialize(T2CKPS prescaler, U8 period)
 {
-	timer2_enable(false);         // disable timer
+	timer2_enable(false);        // disable timer
 	T2CONbits.CKPS = T2CKPS_1;   // clock prescaler
-	T2CLKCONbits.CS = 0b0001;     // Fosc/4 clock source
-	T2PR = period;                // timer period
-	PIE4bits.TMR2IE = 0;          // disable interrupt
-	PIR4bits.TMR2IF = 0;          // reset interrupt flag
+	T2CLKCONbits.CS = 0b0001;    // Fosc/4 clock source
+	T2PR = period;               // timer period
+	timer2_interrupt(false);     // disable interrupt
+	PIR4bits.TMR2IF = 0;         // reset interrupt flag
 }
 
 void timer2_enable(bool enable)
 {
 	T2CONbits.ON = enable;
+}
+
+void timer2_interrupt(bool enable)
+{
+	PIE4bits.TMR2IE = enable;
 }
--- firmware/peripherals/timer2.h
+++ firmware/peripherals/timer2.h
@@ -11,17 +11,18 @@ typedef enum T2CKPS T2CKPS;
  * Used to prescale clock input. */
 enum T2CKPS
 {
-	T2CKPS_128 = 0b111,   // 1:128 prescaler
-	T2CKPS_64  = 0b110,   // 1:64 prescaler
-	T2CKPS_32  = 0b101,   // 1:32 prescaler
-	T2CKPS_16  = 0b100,   // 1:16 prescaler
-	T2CKPS_8   = 0b011,   // 1:8 prescaler
-	T2CKPS_4   = 0b010,   // 1:4 prescaler
-	T2CKPS_2   = 0b001,   // 1:2 prescaler
-	T2CKPS_1   = 0b000    // 1:1 prescaler
+	T2CKPS_128 = 0b111,   // 1:128
+	T2CKPS_64  = 0b110,   // 1:64
+	T2CKPS_32  = 0b101,   // 1:32
+	T2CKPS_16  = 0b100,   // 1:16
+	T2CKPS_8   = 0b011,   // 1:8
+	T2CKPS_4   = 0b010,   // 1:4
+	T2CKPS_2   = 0b001,   // 1:2
+	T2CKPS_1   = 0b000    // 1:1
 };
 
 void timer2_initialize(T2CKPS prescaler, U8 period);
 void timer2_enable(bool enable);
+void timer2_interrupt(bool enable);
 
 #endif /* TIMER2_H */
