01ec71fb7d6c697e3c02aac120c7186109e45311
bricktrooper <kylepinto1018@gmail.com>
2023-07-19 15:52:36 -0400

    Relocate CLI functions

--- software/rainbow.py
+++ software/rainbow.py
@@ -3,9 +3,14 @@
 import log
 import uart
 import link
+import struct
 
+from log import colours
+from link import Opcode
+from os.path import basename
 from cli import Command, ERROR, SUCCESS
 from sys import argv
+from rgb import RGB
 
 # ===================== UTILITIES ===================== #
 
@@ -24,25 +29,72 @@ def parse_unsigned(name, string, min, max):
 
 	return value
 
-# ===================== SUBCOMMANDS ===================== #
+# ===================== REQUESTS ===================== #
+
+def ping():
+	log.info(f"PING")
+	if link.request(Opcode.PING, []) == ERROR:
+		return ERROR
+	if link.listen() == ERROR:
+		return ERROR
+	return SUCCESS
+
+def rainbow(speed):
+	log.info(f"RAINBOW {colours.YELLOW}speed{colours.RESET}: {speed}")
+	payload = struct.pack("<B", speed)
+	if link.request(Opcode.RAINBOW, payload) == ERROR:
+		return ERROR
+	if link.listen() == ERROR:
+		return ERROR
+	return SUCCESS
+
+def colour(red, green, blue):
+	rgb = RGB(red, green, blue)
+	log.info(f"COLOUR {rgb}")
+	payload = rgb.pack()
+	if link.request(Opcode.COLOUR, payload) == ERROR:
+		return ERROR
+	if link.listen() == ERROR:
+		return ERROR
+	return SUCCESS
+
+def brightness(red, green, blue):
+	rgb = RGB(red, green, blue)
+	log.info(f"BRIGHTNESS {rgb}")
+	payload = rgb.pack()
+	if link.request(Opcode.BRIGHTNESS, payload) == ERROR:
+		return ERROR
+	if link.listen() == ERROR:
+		return ERROR
+	return SUCCESS
+
+def reboot():
+	if link.request(Opcode.REBOOT, []) == ERROR:
+		return ERROR
+	if link.listen() == ERROR:
+		return ERROR
+	return SUCCESS
+
+# ===================== CLI SUBCOMMANDS ===================== #
 
 MAX_PINGS = 100
 
-def ping(prefix, args):
+def cli_ping(prefix, args):
 	count = 1
 	if len(args) > 0:
 		count = parse_unsigned("count", args.pop(0), 0, MAX_PINGS)
 		if count == ERROR:
 			return ERROR
 
+	log.info(f"PING {colours.MAGENTA}count{colours.RESET}: {count}")
 	uart.connect()
 	for i in range(count):
-		if link.ping() == ERROR:
+		if ping() == ERROR:
 			return ERROR
 	uart.disconnect()
 	return SUCCESS
 
-def colour(prefix, args):
+def cli_colour(prefix, args):
 	red = args.pop(0)
 	green = args.pop(0)
 	blue = args.pop(0)
@@ -58,21 +110,21 @@ def colour(prefix, args):
 		return ERROR
 
 	uart.connect()
-	result = link.colour(red, green, blue)
+	result = colour(red, green, blue)
 	uart.disconnect()
 	return result
 
-def rainbow(prefix, args):
+def cli_rainbow(prefix, args):
 	speed = parse_unsigned("speed", args.pop(0), 0, 255)
 	if speed == ERROR:
 		return ERROR
 
 	uart.connect()
-	result = link.rainbow(speed)
+	result = rainbow(speed)
 	uart.disconnect()
 	return result
 
-def brightness(prefix, args):
+def cli_brightness(prefix, args):
 	red = args.pop(0)
 	green = args.pop(0)
 	blue = args.pop(0)
@@ -88,13 +140,13 @@ def brightness(prefix, args):
 		return ERROR
 
 	uart.connect()
-	result = link.brightness(red, green, blue)
+	result = brightness(red, green, blue)
 	uart.disconnect()
 	return result
 
-def reboot(prefix, args):
+def cli_reboot(prefix, args):
 	uart.connect()
-	result = link.reboot()
+	result = reboot()
 	uart.disconnect()
 	return result
 
@@ -102,15 +154,15 @@ def reboot(prefix, args):
 
 def main():
 	log.suppress(log.Level.DEBUG)
-	program = argv.pop(0)
+	program = basename(argv.pop(0))
 	prefix = f"{program}"
 
-	command = Command(prefix, 1)
-	command.leaf(ping, "ping", "[count]", 0, 1)
-	command.leaf(colour, "colour", "<red> <green> <blue>", 3, 3)
-	command.leaf(brightness, "brightness", "<red> <green> <blue>", 3, 3)
-	command.leaf(rainbow, "rainbow", "<speed>", 1, 1)
-	command.leaf(reboot, "reboot", "", 0, 0)
+	command = Command("rainbow", 1)
+	command.leaf(cli_ping,       "ping",       "[count]",              0, 1, "Ping the controller")
+	command.leaf(cli_colour,     "colour",     "<red> <green> <blue>", 3, 3, "Set the RGB colour")
+	command.leaf(cli_brightness, "brightness", "<red> <green> <blue>", 3, 3, "Set the maximum brightness for each RGB channel")
+	command.leaf(cli_rainbow,    "rainbow",    "<speed>",              1, 1, "Cycle through the colours in a rainbow")
+	command.leaf(cli_reboot,     "reboot",     "",                     0, 0, "Reboot the controller")
 
 	result = command.run(argv)
 	exit(result)
