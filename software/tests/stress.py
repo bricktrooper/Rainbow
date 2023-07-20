bed25f10b23bb8fdfce50b85e9ca08b54c3664d3
bricktrooper <kylepinto1018@gmail.com>
2023-07-20 00:14:38 -0400

    Test scripts

---
+++ software/tests/stress.py
@@ -0,0 +1,18 @@
+#! /usr/local/bin/python3
+
+import rainbow
+import log
+import time
+
+PORT = "/dev/cu.rainbow-kyle-DevB"
+
+log.suppress(log.Level.DEBUG)
+
+rainbow.connect(PORT)
+
+rainbow.brightness(255, 255, 255)
+rainbow.cycle(255)
+rainbow.ping(100)
+rainbow.off()
+
+rainbow.disconnect()
---
+++ software/tests/sweep.py
@@ -0,0 +1,23 @@
+#! /usr/local/bin/python3
+
+import rainbow
+import log
+
+PORT = "/dev/cu.rainbow-kyle-DevB"
+
+log.suppress(log.Level.DEBUG)
+
+rainbow.connect(PORT)
+
+rainbow.brightness(255, 255, 255)
+
+for red in range(256):
+	rainbow.colour(red, 0, 0)
+for green in range(256):
+	rainbow.colour(0, green, 0)
+for blue in range(256):
+	rainbow.colour(0, 0, blue)
+
+rainbow.off()
+
+rainbow.disconnect()
