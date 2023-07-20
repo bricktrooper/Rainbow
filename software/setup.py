f9dc6889e67fd7704e4a3dc79872b4457c53f13d
bricktrooper <kylepinto1018@gmail.com>
2023-07-20 00:14:03 -0400

    setup.py for pip installation

---
+++ software/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = "rainbow",
+	version = "0.0",
+	description = "API for interacting with the Rainbow controller board.",
+	author = "Kyle Pinto",
+	author_email = "kylepinto1018@gmail.com",
+	packages = ["rainbow"]
+)
